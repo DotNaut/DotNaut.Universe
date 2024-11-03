8e23437abd2b295a81267b1cce8da2f5c9066054
stan <stanislav@yarmonov.pro>
2024-11-03 12:04:41 +0200

    +INamed interface

---
+++ DotNaut/Ontology/INamed.cs
@@ -0,0 +1,10 @@
+namespace DotNaut.Ontology;
+
+/// <remarks>
+/// Follows the adjective pattern like other common interfaces (IComparable, IDisposable,
+/// IQueryable).
+/// </remarks>
+public interface INamed
+{
+    string Get();
+}
