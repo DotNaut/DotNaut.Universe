9c817eca1fb57360e16f0ad0ed09313ad10e6972
stan <stanislav@yarmonov.pro>
2024-11-06 19:31:53 +0200

    +ISqlConvention idea

---
+++ DotNaut/Data/Sql/ISqlConvention.cs
@@ -0,0 +1,15 @@
+using DotNaut.Application.Flow;
+
+namespace DotNaut.Data.Sql;
+
+public interface ISqlConvention<TRequest, TProduct>
+    : IProcess<TRequest, TProduct>
+{
+
+}
+
+public interface ISqlConvention<TRequest>
+    : IProcess<TRequest, string>
+{
+
+}
