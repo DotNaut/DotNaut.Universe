f7e46458ba6a75fab6872c4cba2d3a8e2de5a23d
stan <stanislav@yarmonov.pro>
2024-11-25 10:22:11 +0200

    +SqlHelper primitive

---
+++ DotNaut.Specs.Components/Content/SqlHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace DotNaut.Content;
+
+/// <summary>
+/// TODO:A big mess for a while (location, responsibility), but the first baby step.
+/// </summary>
+public class SqlHelper
+{
+    public static string ReadResourceAsString(Type type, string name)
+    {
+        using var stream = type.Assembly.GetManifestResourceStream(type, name);
+        if (stream == null)
+        {
+            throw new ArgumentException($"Resource `{name}` not found for the {type.FullName}");
+        }
+
+        using var reader = new StreamReader(stream);
+
+        return reader.ReadToEnd();
+    }
+}
