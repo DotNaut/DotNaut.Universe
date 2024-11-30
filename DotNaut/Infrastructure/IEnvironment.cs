d00fce16a3b57576f909f6c0ccf83f6340ce1b0e
stan <stanislav@yarmonov.pro>
2024-11-30 09:55:07 +0200

    +Infrastructure environment basics

---
+++ DotNaut/Infrastructure/IEnvironment.cs
@@ -0,0 +1,12 @@
+using DotNaut.Ontology;
+
+namespace DotNaut.Infrastructure;
+
+/// <remarks>
+/// TODO:Kind of a layers for the infrastructure.
+/// </remarks>
+public interface IEnvironment
+	: INamed
+{
+
+}
